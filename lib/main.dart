5bf4759bf2f2bd98e9310fa44213a09899becee2
AdityaKuchhal <adikuchhal@gmail.com>
2021-10-07 21:12:08 +0530

    asd

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:doctor_app/screens/welcome/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        // systemNavigationBarColor: Colors.black,
+        systemNavigationBarIconBrightness: Brightness.dark,
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+    );
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Healthcare - Doctor Consultation App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -1,15 +1,70 @@
+import 'package:doctor_app/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(
-          Icons.access_alarm,
-        ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          SvgPicture.asset(
+            "assets/icons/splash_bg.svg",
+          ),
+          SafeArea(
+            child: Column(
+              children: [
+                SvgPicture.asset(
+                  "assets/icons/gerda_logo.svg",
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 14,
+                  ),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color.fromRGBO(52, 63, 86, .2),
+                            blurRadius: 20,
+                            offset: Offset(0, 10),
+                          ),
+                        ],
+                        borderRadius: BorderRadius.circular(10),
+                        gradient: LinearGradient(
+                          colors: [
+                            Color.fromRGBO(245, 71, 72, .8),
+                            Color.fromRGBO(245, 71, 72, 1),
+                          ],
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Send OTP',
+                          style: TextStyle(
+                            fontFamily: 'Nunito',
+                            fontSize: screenHeight / 45,
+                            fontWeight: FontWeight.bold,
+                            color: kWhite,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
